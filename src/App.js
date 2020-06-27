07021118e24c9fb385db2875757232b3f3b5c8b7
Franci Nakamura <franci.nakamura100@gmail.com>
2020-06-27 08:38:58 -0700

    initiate app and add event handler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function handleClick() {
+  console.log("I was clicked");
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        onMouseOver={() => console.log("Hovering over Bill")}
+        src="https://www.fillmurray.com/300/200"
+        alt=" "
+      />
+      <br />
+      <br />
+      <button onClick={handleClick}> Click Here</button>
     </div>
   );
 }
